cf5513f3868fe47baf73bdb7c3c5a50261d2a43f
Andrey-78 <avk11121978@mail.ru>
2025-03-15 08:07:22 +0300

    merge part_25

--- Popcorn/Ball.cpp
+++ Popcorn/Ball.cpp
@@ -113,7 +113,7 @@ void ABall::Set_State(EBall_State new_state, double x_pos)
 	}
 
 	Ball_State = new_state;
-}	
+}
 //------------------------------------------------------------------------------------------------------------
 double ABall::Get_Direction()
 {
@@ -124,18 +124,18 @@ void ABall::Set_Direction(double new_direction)
 {
 	const double pi_2 = 2.0 * M_PI;
 
-	while(new_direction >= pi_2)
+	while (new_direction > pi_2)
 		new_direction -= pi_2;
 
-	while(new_direction < 0)
+	while (new_direction < 0.0)
 		new_direction += pi_2;
-	
+
 	Ball_Direction = new_direction;
 }
 //------------------------------------------------------------------------------------------------------------
-void ABall::Reflect(bool from_gorisontal)
+void ABall::Reflect(bool from_horizontal)
 {
-	if(from_gorisontal)
+	if (from_horizontal)
 		Set_Direction(-Ball_Direction);
 	else
 		Set_Direction(M_PI - Ball_Direction);
--- Popcorn/Level.cpp
+++ Popcorn/Level.cpp
@@ -44,24 +44,23 @@ bool ALevel::Check_Hit(double next_x_pos, double next_y_pos, ABall *ball)
 				continue;
 
 			brick_left_x = AsConfig::Level_X_Offset + j * AsConfig::Cell_Width;
-			brick_right_x = brick_left_x + AsConfig::Brick_Width; 
+			brick_right_x = brick_left_x + AsConfig::Brick_Width;
 
 			// Проверяем попадание в нижнюю грань
 			if (direction >= 0 && direction < M_PI)
-			if(Hit_Circle_On_Line(next_y_pos - brick_low_y, next_x_pos, brick_left_x, brick_right_x, ball->Radius) )
-			{
-				ball->Reflect(true);
-				return true;
-			}
+				if (Hit_Circle_On_Line(next_y_pos - brick_low_y, next_x_pos, brick_left_x, brick_right_x, ball->Radius) )
+				{
+					ball->Reflect(true);
+					return true;
+				}
 
 			// Проверяем попадание в верхнюю грань
-			if(direction >= M_PI && direction <= 2.0 * M_PI)
-			if(Hit_Circle_On_Line(next_y_pos - brick_top_y, next_x_pos, brick_left_x, brick_right_x, ball->Radius) )
-			{
-				ball->Reflect(true);
-				return true;
-			}
-
+			if (direction >= M_PI && direction <= 2.0 * M_PI)
+				if (Hit_Circle_On_Line(next_y_pos - brick_top_y, next_x_pos, brick_left_x, brick_right_x, ball->Radius) )
+				{
+					ball->Reflect(true);
+					return true;
+				}
 		}
 	}
 
@@ -98,23 +97,27 @@ void ALevel::Draw(HDC hdc, RECT &paint_area)
 }
 //------------------------------------------------------------------------------------------------------------
 bool ALevel::Hit_Circle_On_Line(double y, double next_x_pos, double left_x, double right_x, double radius)
-{// проверяет пересечение горизонтального отрезка (проходящего от left_x до right_x через y) с окружностью радиусом radius
+{// Проверяет пересечение горизонтального отрезка (проходящего от left_x до right_x через y) с окружностью радиусом radius
 
 	double x;
 	double min_x, max_x;
-	
-		if(y > radius)
+
+	// x * x + y * y = R * R
+	// x = sqrt(R * R - y * y)
+	// y = sqrt(R * R - x * x)
+
+	if (y > radius)
 		return false;
 
 	x = sqrt(radius * radius - y * y);
 
-	min_x = next_x_pos - x;
 	max_x = next_x_pos + x;
+	min_x = next_x_pos - x;
 
-	if (max_x >= left_x && max_x <= right_x || min_x >= left_x && min_x <= right_x)
+	if (max_x >= left_x && max_x <= right_x  ||  min_x >= left_x && min_x <= right_x)
 		return true;
 	else
-		false;
+		return false;
 }
 //------------------------------------------------------------------------------------------------------------
 void ALevel::Draw_Brick(HDC hdc, int x, int y, EBrick_Type brick_type)
